62c14c283a5a239ef98ff8927b930cb21cfb9836
DeCraftsman64 <safoqwame99@gmail.com>
2020-06-27 20:57:15 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def index():
+    return render_template('index.html', view='login/login_V20.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('index.html', view='landing/index.html', home='active')
+
+
+@app.route('/about')
+def about():
+    return render_template('index.html', view='landing/about.html', about='active')
+
+
+@app.route('/blog')
+def route():
+    return render_template('index.html', view='landing/blog.html', blog='active')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('index.html', view='landing/contact.html', contact='active')
+
+
+@app.route('/elements')
+def elements():
+    return render_template('index.html', view='landing/elements.html')
+
+
+@app.route('/main')
+def mains():
+    return render_template('index.html', view='landing/main.html')
+
+
+@app.route('/package')
+def package():
+    return render_template('index.html', view='landing/package.html', package='active')
+
+
+@app.route('/search')
+def search():
+    return render_template('index.html', view='landing/search.html')
+
+
+@app.route('/single-blog')
+def single_blog():
+    return render_template('index.html', view='landing/single-blog.html', )
+
+
+@app.route('/support')
+def support():
+    return render_template('index.html', view='landing/support.html', support='active')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('index.html', view='landing/blog.html')
+
+
+@app.route('/intro')
+def intro():
+    return render_template('index.html', view='landing/intro.html', home='active')
+
+
+if __name__ == '__main__':
+    app.run()
