ef2ccc85a83cc9f261415f4b5852cf15cc06efb8
DeCraftsman64 <safoqwame99@gmail.com>
2020-07-02 23:28:34 +0000

    fixed login framework

--- app.py
+++ app.py
@@ -8,71 +8,67 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    msg = render_template('index.html', view='login/login_V20.html')
-    text_file = open("templates/gen/index.html", "wt")
-    n = text_file.write(msg)
-    text_file.close()
-    return msg
+    return render_template('render.html', view='login/index.html')
 
 
 @app.route('/home')
 def home():
-    return render_template('index.html', view='landing/index.html', home='active')
+    return render_template('render.html', view='landing/render.html', home='active')
 
 
 @app.route('/about')
 def about():
-    return render_template('index.html', view='landing/about.html', about='active')
+    return render_template('render.html', view='landing/about.html', about='active')
 
 
 @app.route('/blog')
 def route():
-    return render_template('index.html', view='landing/blog.html', blog='active')
+    return render_template('render.html', view='landing/blog.html', blog='active')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('index.html', view='landing/contact.html', contact='active')
+    return render_template('render.html', view='landing/contact.html', contact='active')
 
 
 @app.route('/elements')
 def elements():
-    return render_template('index.html', view='landing/elements.html')
+    return render_template('render.html', view='landing/elements.html')
 
 
 @app.route('/main')
 def mains():
-    return render_template('index.html', view='landing/main.html')
+    return render_template('render.html', view='landing/main.html')
 
 
 @app.route('/package')
 def package():
-    return render_template('index.html', view='landing/package.html', package='active')
+    return render_template('render.html', view='landing/package.html', package='active')
 
 
 @app.route('/search')
 def search():
-    return render_template('index.html', view='landing/search.html')
+    return render_template('render.html', view='landing/search.html')
 
 
 @app.route('/single-blog')
 def single_blog():
-    return render_template('index.html', view='landing/single-blog.html', )
+    return render_template('render.html', view='landing/single-blog.html', )
 
 
 @app.route('/support')
 def support():
-    return render_template('index.html', view='landing/Support.html', support='active')
+    return render_template('render.html', view='landing/Support.html', support='active')
 
 
 @app.route('/blog')
 def blog():
-    return render_template('index.html', view='landing/blog.html')
+    return render_template('render.html', view='landing/blog.html')
 
 
 @app.route('/intro')
 def intro():
-    return render_template('index.html', view='landing/intro.html', home='active')
+    return render_template('render.html', view='landing/intro.html', home='active')
 
 
 if __name__ == '__main__':
